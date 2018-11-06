8830d606fa3985f5a56a2b791ceab29626a3f45c
Bilal Eraslan <beraslan@muhasebat.gov.tr>
2018-11-06 15:17:30 +0300

    config dene

--- src/main/java/com/be/apptwo/common/model/Location.java
+++ src/main/java/com/be/apptwo/common/model/Location.java
@@ -8,84 +8,82 @@ import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-
 /**
  * The persistent class for the locations database table.
- * 
  */
 @Entity
-@Table(name="locations")
-@NamedQuery(name="Location.findAll", query="SELECT l FROM Location l")
+@Table(name = "locations")
+@NamedQuery(name = "Location.findAll", query = "SELECT l FROM Location l")
 public class Location implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@Column(name="location_id")
-	private Integer locationId;
+    @Id
+    @Column(name = "location_id")
+    private Integer locationId;
 
-	private String city;
+    private String city;
 
-	@Column(name="country_id" ,columnDefinition = "bpchar(2)")
-	private String countryId;
+    @Column(name = "country_id")
+    private String countryId;
 
-	@Column(name="postal_code")
-	private String postalCode;
+    @Column(name = "postal_code")
+    private String postalCode;
 
-	@Column(name="state_province")
-	private String stateProvince;
+    @Column(name = "state_province")
+    private String stateProvince;
 
-	@Column(name="street_address")
-	private String streetAddress;
+    @Column(name = "street_address")
+    private String streetAddress;
 
-	public Location() {
-	}
+    public Location() {
+    }
 
-	public Integer getLocationId() {
-		return this.locationId;
-	}
+    public Integer getLocationId() {
+        return this.locationId;
+    }
 
-	public void setLocationId(Integer locationId) {
-		this.locationId = locationId;
-	}
+    public void setLocationId(Integer locationId) {
+        this.locationId = locationId;
+    }
 
-	public String getCity() {
-		return this.city;
-	}
+    public String getCity() {
+        return this.city;
+    }
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	public String getCountryId() {
-		return this.countryId;
-	}
+    public String getCountryId() {
+        return this.countryId;
+    }
 
-	public void setCountryId(String countryId) {
-		this.countryId = countryId;
-	}
+    public void setCountryId(String countryId) {
+        this.countryId = countryId;
+    }
 
-	public String getPostalCode() {
-		return this.postalCode;
-	}
+    public String getPostalCode() {
+        return this.postalCode;
+    }
 
-	public void setPostalCode(String postalCode) {
-		this.postalCode = postalCode;
-	}
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
-	public String getStateProvince() {
-		return this.stateProvince;
-	}
+    public String getStateProvince() {
+        return this.stateProvince;
+    }
 
-	public void setStateProvince(String stateProvince) {
-		this.stateProvince = stateProvince;
-	}
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
 
-	public String getStreetAddress() {
-		return this.streetAddress;
-	}
+    public String getStreetAddress() {
+        return this.streetAddress;
+    }
 
-	public void setStreetAddress(String streetAddress) {
-		this.streetAddress = streetAddress;
-	}
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
 
 }
