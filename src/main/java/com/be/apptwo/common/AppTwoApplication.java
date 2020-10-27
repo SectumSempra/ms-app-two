a6f095978f25f38eb0147794c72a5a404ada3b98
bilaleraslan <bilaleraslan@gmail.com>
2020-10-27 17:29:58 +0300

    duzenleme

--- src/main/java/com/be/apptwo/common/AppTwoApplication.java
+++ src/main/java/com/be/apptwo/common/AppTwoApplication.java
@@ -16,32 +16,34 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class AppTwoApplication {
 
+    private static final String ipPrefix = "192.";
+
     public static Map<String, Object> getMap() throws Exception {
-        Map<String, Object> map = new HashMap<>();
-        String ip = getIpAdressBy10();
-        map.put("ip", ip);
-        return map;
+	Map<String, Object> map = new HashMap<>();
+	String ip = getIpAdressBy10();
+	map.put("ip", ip);
+	return map;
 
     }
 
     private static String getIpAdressBy10() throws SocketException {
-        Enumeration e = NetworkInterface.getNetworkInterfaces();
-        while (e.hasMoreElements()) {
-            NetworkInterface n = (NetworkInterface) e.nextElement();
-            Enumeration ee = n.getInetAddresses();
-            while (ee.hasMoreElements()) {
-                InetAddress i = (InetAddress) ee.nextElement();
-                if (i.getHostAddress().startsWith("10."))
-                    return i.getHostAddress();
-            }
-        }
-        return null;
+	Enumeration e = NetworkInterface.getNetworkInterfaces();
+	while (e.hasMoreElements()) {
+	    NetworkInterface n = (NetworkInterface) e.nextElement();
+	    Enumeration ee = n.getInetAddresses();
+	    while (ee.hasMoreElements()) {
+		InetAddress i = (InetAddress) ee.nextElement();
+		if (i.getHostAddress().contains(ipPrefix))
+		    return i.getHostAddress();
+	    }
+	}
+	return null;
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication application = new SpringApplication(AppTwoApplication.class);
-        application.setDefaultProperties(getMap());
-        application.run(args);
+	SpringApplication application = new SpringApplication(AppTwoApplication.class);
+	application.setDefaultProperties(getMap());
+	application.run(args);
 
     }
 }
